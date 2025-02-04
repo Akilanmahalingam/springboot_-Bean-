072e36fea3f0e534cb8499ea3fb2f1f9e86f1dc0
akilanmahalingam <akilansundhar@gmail.com>
2025-02-04 19:10:26 +0530

    Setter Injection streotype annotation

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -6,15 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Data
+
 @Component
 public class Student {
 
     private int rno; // default value is zero
 
-    @Autowired
-    @Qualifier("pen")
     private Writer writer; // default value is null // Has to given new Pen()n, new Pencil , objects are ready , just need to link
+
+    public Writer getWriter() {
+        return writer;
+    }
+    @Autowired
+    @Qualifier("pencil")
+    public void setWriter(Writer writer) {
+        System.out.println("Through setter method");
+        this.writer = writer;
+    }
+
+    public int getRno() {
+        return rno;
+    }
+
+    public void setRno(int rno) {
+        this.rno = rno;
+    }
 
     public Student()
     {
